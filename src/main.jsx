eb9ea27245df7cf3cd275c6c60da766da23e2193
NITESH KUMAR <rnitesh4445@gmail.com>
2025-10-26 19:02:59 +0530

    second

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import {Index} from './component';
+import {Index} from './component/Routes';
 import { CookiesProvider } from 'react-cookie';
 
 
